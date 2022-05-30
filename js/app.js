8f47ed81f910c19d9d1653dabf10925c522d1159
stacksantos <stacksantos@proton.me>
2022-05-30 14:03:17 -0500

    update nav js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 const dropDowns = document.querySelectorAll(".dropDown");
 const button = document.querySelector("#dropButton");
-const navLinks = document.querySelectorAll(".navLink");
+const navLinks = document.querySelectorAll(".navLink li a");
 const menu = document.querySelector(".menu");
 const menuOpen = document.querySelector(".menu-open");
 
 
-button.addEventListener("mousedown", () => {
-  menu.classList.toggle("menu-open");
-});
+// button.addEventListener('mousedown', () => {
+//   menu.classList.toggle("menu-open");
+// });
 
-navLinks.forEach(navlink => {
-    navlink.addEventListener('mousedown', () => {
+// navLinks.forEach(navlink => {
+//     navlink.addEventListener('mousedown', () => {
+//         menu.classList.toggle('menu-open');
+//     })
+// })
 
-        menu.classList.toggle('menu-open');
-    })
-})
+    button.addEventListener('mousedown', () => {
+        if (window.getComputedStyle(menu).display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
+        window.addEventListener('scroll', () => {
+            menu.style.display = 'none';
+        })
+    });
+
+    navLinks.forEach(navlink => {
+            
+    });
