059a26de22c8d0532e1e0519eae803b61fa042c2
stacksantos <stacksantos@proton.me>
2022-05-30 12:21:00 -0500

    adjust nav js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const dropDowns = document.querySelectorAll('.dropDown');
-const button = document.querySelector('#dropButton');
-const menu = document.querySelector('.menu');
-const option = document.querySelector('.menu li');
+const dropDowns = document.querySelectorAll(".dropDown");
+const button = document.querySelector("#dropButton");
+const navLinks = document.querySelectorAll(".navLink");
+const menu = document.querySelector(".menu");
+const menuOpen = document.querySelector(".menu-open");
 
 
-button.addEventListener('mousedown', () => {
-    menu.classList.toggle('menu-open');
+button.addEventListener("mousedown", () => {
+  menu.classList.toggle("menu-open");
 });
 
+navLinks.forEach(navlink => {
+    navlink.addEventListener('mousedown', () => {
+
+        menu.classList.toggle('menu-open');
+    })
+})
