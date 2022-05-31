7725c7ff691b2e67359e357814f112ecb63fbed3
stacksantos <stacksantos@proton.me>
2022-05-31 00:32:56 -0500

    update responsiveness

--- js/app.js
+++ js/app.js
@@ -1,31 +1,13 @@
-const dropDowns = document.querySelectorAll(".dropDown");
 const button = document.querySelector("#dropButton");
-const navLinks = document.querySelectorAll(".navLink li a");
 const menu = document.querySelector(".menu");
-const menuOpen = document.querySelector(".menu-open");
 
-
-// button.addEventListener('mousedown', () => {
-//   menu.classList.toggle("menu-open");
-// });
-
-// navLinks.forEach(navlink => {
-//     navlink.addEventListener('mousedown', () => {
-//         menu.classList.toggle('menu-open');
-//     })
-// })
-
-    button.addEventListener('mousedown', () => {
-        if (window.getComputedStyle(menu).display === 'block') {
-            menu.style.display = 'none';
-        } else {
-            menu.style.display = 'block';
-        }
-        window.addEventListener('scroll', () => {
-            menu.style.display = 'none';
-        })
+button.addEventListener("mousedown", () => {
+  if (window.getComputedStyle(menu).display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    window.addEventListener("scroll", () => {
+      menu.style.display = "none";
     });
-
-    navLinks.forEach(navlink => {
-            
-    });
+  }
+});
