2692cd9d2d1a33365f1409b3a5fcd17956ae2ba9
stacksantos <stacksantos@proton.me>
2022-06-14 20:12:50 -0500

    fix the things I messed up

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 /* -------------------------------------------------------------------------- */
 /*                                                                            */
 /* -------------------------------------------------------------------------- */
 
 // for mobile menu
 
-=======
->>>>>>> d05a863d188b95dc1d1a3437e0d3470c777dc9c3
 const button = document.querySelector("#dropButton");
 const menu = document.querySelector(".menu");
 
@@ -20,7 +17,6 @@ button.addEventListener("mousedown", () => {
     });
   }
 });
-<<<<<<< HEAD
 
 // change active class on full size menu
 
@@ -38,57 +34,55 @@ navItems.forEach((navItem) =>
 /* -------------------------------------------------------------------------- */
 /*                          SECTION SCROLL INTO VIEW                          */
 /* -------------------------------------------------------------------------- */
-const scrollSections = document.querySelectorAll(".scroll")
-const scrollSection = document.querySelector(".scroll");
-const scrollOffset = 100;
+// const scrollSections = document.querySelectorAll(".scroll")
+// const scrollSection = document.querySelector(".scroll");
+// const scrollOffset = 100;
 
-scrollSections.forEach((section) => {
-  section.style.opacity = 0;
-});
+// scrollSections.forEach((section) => {
+//   section.style.opacity = 0;
+// });
 
-const sectionInView = (section, dividend = 1) => {
-  const sectionTop = section.getBoundingClientRect().top;
+// const sectionInView = (section, dividend = 1) => {
+//   const sectionTop = section.getBoundingClientRect().top;
 
-  return (
-    sectionTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-    );
-};
+//   return (
+//     sectionTop <=
+//     (window.innerHeight || document.documentElement.clientHeight) / dividend
+//     );
+// };
 
-const sectionOutofView = (section) => {
-  const sectionTop = section.getBoundingClientRect().top;
+// const sectionOutofView = (section) => {
+//   const sectionTop = section.getBoundingClientRect().top;
 
-  return (
-    sectionTop > (window.innerHeight ||
-      document.documentElement.clientHeight)
-  );
-};
+//   return (
+//     sectionTop > (window.innerHeight ||
+//       document.documentElement.clientHeight)
+//   );
+// };
 
-const displayScrolledSection = (section) => {
-  section.classList.add("intoView");
-};
-const hideScrolledSection = (section) => {
-  section.classList.remove("intoView");
-};
+// const displayScrolledSection = (section) => {
+//   section.classList.add("intoView");
+// };
+// const hideScrolledSection = (section) => {
+//   section.classList.remove("intoView");
+// };
 
-const handleScrollAnimation = () => {
-  scrollSections.forEach((section) => {
-    if (sectionInView(scrollSection, scrollOffset)) {
-      displayScrolledSection(scrollSection);
-    } else if (sectionOutofView(section)) {
-      hideScrolledSection(section)
-    }
-  })
-};
+// const handleScrollAnimation = () => {
+//   scrollSections.forEach((section) => {
+//     if (sectionInView(scrollSection, scrollOffset)) {
+//       displayScrolledSection(scrollSection);
+//     } else if (sectionOutofView(section)) {
+//       hideScrolledSection(section)
+//     }
+//   })
+// };
 
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
-});
+// window.addEventListener("scroll", () => {
+//   handleScrollAnimation();
+// });
 
 
 
 /* -------------------------------------------------------------------------- */
 /*                                     END                                    */
 /* -------------------------------------------------------------------------- */
-=======
->>>>>>> d05a863d188b95dc1d1a3437e0d3470c777dc9c3
